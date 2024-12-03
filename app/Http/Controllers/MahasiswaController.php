45eada9a00ca2fb2641aeae8379eb3ea898f8237
Ikhwan <ikhwanpratamahidayat@gmail.com>
2024-12-03 11:16:51 +0700

    commit ke 2 (revisi)

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'npm' => 'required',
+            'nama' => 'required',
+            'prodi' => 'required',
+        ]);
+    
+        $mahasiswa = new Mahasiswa();
+        $mahasiswa->npm = $request->npm;
+        $mahasiswa->nama = $request->nama;
+        $mahasiswa->prodi = $request->prodi;
+        $mahasiswa->save();
+    
+        return redirect()->route('mahasiswa.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_12_03_035901_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->string('nama'); // Nama Lengkap mahasiswa
+            $table->string('npm')->unique(); // Nomor Pokok Mahasiswa, unik
+            $table->string('prodi'); // Prodi mahasiswa
+            $table->timestamps(); // Kolom created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
